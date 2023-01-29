39f76acd85af2ab18719d16478e7bdb72a9b8e40
david <david.leon-m@mail.escuelaing.edu.co>
2023-01-29 17:48:52 -0500

    falta cache

--- src/main/java/URI/HttpServer.java
+++ src/main/java/URI/HttpServer.java
@@ -5,7 +5,10 @@ import java.io.*;
 import java.security.PublicKey;
 
 public class HttpServer {
+    public static URLreader busquedaAPI;
     public static void main(String[] args) throws IOException {
+        Socket clientSocket = null;
+        busquedaAPI = new URLreader();
         ServerSocket serverSocket = null;
         try {
             serverSocket = new ServerSocket(35000);
@@ -16,37 +19,38 @@ public class HttpServer {
         boolean running = true;
         int cont = 0;
         while(running) {
-            Socket clientSocket = null;
             try {
-                System.out.println("Listo para recibir ...");
+                System.out.println("RECIBIENDO PETICIONES");
                 clientSocket = serverSocket.accept();
+                if(cont == 0){
+                    front(clientSocket);
+                    cont +=1;
+                }
+                busqueda(clientSocket);
             } catch (IOException e) {
                 System.err.println("Accept failed.");
                 System.exit(1);
             }
-
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(
-                            clientSocket.getInputStream()));
-            String inputLine, outputLine;
-
-            while ((inputLine = in.readLine()) != null) {
-                System.out.println("Received: " + inputLine);
-                if (!in.ready()) {
-                    break;
-                }
-            }
-            outputLine = "HTTP/1.1 200 OK\r\n" + "Content-Type:  text/html\r\n" + "\r\n" + htmlForm();
-            if(cont == 0) {
-                out.println(outputLine);
-                cont +=1;
-            }
-            out.close();
-            in.close();
-            clientSocket.close();
         }
-        serverSocket.close();
+    }
+    public static void front(Socket clientSocket) throws IOException {
+        String outputLine;
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+        outputLine = "HTTP/1.1 200 OK\r\n" + "Content-Type:  text/html\r\n" + "\r\n" + htmlForm();
+        out.println(outputLine);
+    }
+    public static void busqueda(Socket clientSocket) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        String[] datos = in.readLine().split(" ");
+        if(datos[0].equals("POST")){
+            String res = busquedaAPI.getAPI(datos[1].substring(1));
+            System.out.println(res);
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            String outputLine = "HTTP/1.1 200 OK\r\n" + "Content-Type:  application/json\r\n" + "\r\n" + res;
+            out.println(outputLine);
+        }
+
+        in.close();
     }
     public static String htmlForm(){
         return "<!DOCTYPE html>\n" +
@@ -57,28 +61,8 @@ public class HttpServer {
                 "        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
                 "    </head>\n" +
                 "    <body>\n" +
-                "        <h1>Form with GET</h1>\n" +
-                "        <form action=\"/hello\">\n" +
-                "            <label for=\"name\">Name:</label><br>\n" +
-                "            <input type=\"text\" id=\"name\" name=\"name\" value=\"John\"><br><br>\n" +
-                "            <input type=\"button\" value=\"Submit\" onclick=\"loadGetMsg()\">\n" +
-                "        </form> \n" +
-                "        <div id=\"getrespmsg\"></div>\n" +
                 "\n" +
-                "        <script>\n" +
-                "            function loadGetMsg() {\n" +
-                "                let nameVar = document.getElementById(\"name\").value;\n" +
-                "                const xhttp = new XMLHttpRequest();\n" +
-                "                xhttp.onload = function() {\n" +
-                "                    document.getElementById(\"getrespmsg\").innerHTML =\n" +
-                "                    this.responseText;\n" +
-                "                }\n" +
-                "                xhttp.open(\"GET\", \"/hello?name=\"+nameVar);\n" +
-                "                xhttp.send();\n" +
-                "            }\n" +
-                "        </script>\n" +
-                "\n" +
-                "        <h1>Form with POST</h1>\n" +
+                "        <h1>INGRESE EL NOMBRE DE LA PELICULA</h1>\n" +
                 "        <form action=\"/hellopost\">\n" +
                 "            <label for=\"postname\">Name:</label><br>\n" +
                 "            <input type=\"text\" id=\"postname\" name=\"name\" value=\"John\"><br><br>\n" +
@@ -89,7 +73,7 @@ public class HttpServer {
                 "        \n" +
                 "        <script>\n" +
                 "            function loadPostMsg(name){\n" +
-                "                let url = \"/hellopost?name=\" + name.value;\n" +
+                "                let url = \"\" + name.value;\n" +
                 "\n" +
                 "                fetch (url, {method: 'POST'})\n" +
                 "                    .then(x => x.text())\n" +
--- src/main/java/URI/URLreader.java
+++ src/main/java/URI/URLreader.java
@@ -4,26 +4,15 @@ import java.io.*;
 import java.net.*;
 
 public class URLreader {
-    public static void main(String[] args) throws Exception {
-        URL myurl = new URL("https://david.escuelaing.edu.co:80/index.html?hola=mundo#2023-1");
-        URL google = new URL("http://www.google.com/");
-        System.out.println("Protocolo: " + myurl.getProtocol());
-        System.out.println("Authority: " + myurl.getAuthority());
-        System.out.println("Host: " + myurl.getHost());
-        System.out.println("Port: " + myurl.getPort());
-        System.out.println("Path: " + myurl.getPath());
-        System.out.println("Query: " + myurl.getQuery());
-        System.out.println("File: " + myurl.getFile());
-        System.out.println("Ref: " + myurl.getRef());
-        System.out.println("-------------------------------------------------------------------------");
-        try (BufferedReader reader
-                     = new BufferedReader(new InputStreamReader(google.openStream()))) {
-            String inputLine = null;
-            while ((inputLine = reader.readLine()) != null) {
-                System.out.println(inputLine);
-            }
+    public static String getAPI(String busqueda) throws MalformedURLException {
+        String uri = "https://www.omdbapi.com/?t=" + busqueda +"&apikey=ec2a8fe1";
+        URL api = new URL(uri);
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(api.openStream()))) {
+            String inputLine = reader.readLine();
+            return inputLine;
         } catch (IOException x) {
             System.err.println(x);
         }
+        return null;
     }
 }
