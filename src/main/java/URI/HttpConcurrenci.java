573c2d5fc4b97dc7e34935e1529243af80a6281b
david <david.leon-m@mail.escuelaing.edu.co>
2023-01-31 20:50:22 -0500

    Documentacion

---
+++ src/main/java/URI/HttpConcurrenci.java
@@ -0,0 +1,65 @@
+package URI;
+
+import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executor;
+
+public class HttpConcurrenci {
+    public static void main(String[] args) throws IOException {
+        HttpServer servidor = new HttpServer() {
+            @Override
+            public void bind(InetSocketAddress addr, int backlog) throws IOException {
+
+            }
+
+            @Override
+            public void start() {
+            }
+
+            @Override
+            public void setExecutor(Executor executor) {
+
+            }
+
+            @Override
+            public Executor getExecutor() {
+                return null;
+            }
+
+            @Override
+            public void stop(int delay) {
+
+            }
+
+            @Override
+            public HttpContext createContext(String path, HttpHandler handler) {
+                return null;
+            }
+
+            @Override
+            public HttpContext createContext(String path) {
+                return null;
+            }
+
+            @Override
+            public void removeContext(String path) throws IllegalArgumentException {
+
+            }
+
+            @Override
+            public void removeContext(HttpContext context) {
+
+            }
+
+            @Override
+            public InetSocketAddress getAddress() {
+                return null;
+            }
+        };
+        servidor.start();
+    }
+}
